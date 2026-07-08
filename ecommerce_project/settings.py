cb7bb0d9efa10e3d13bcb3b6ea63833d541c0eb2
Sumayya <saksumayya@gmail.com>
2026-07-08 16:15:32 +0530

    change celery setting

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -337,8 +337,8 @@ LOGGING = {
 
 
 # Celery Configuration
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.getenv("REDIS_URL")
+CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
