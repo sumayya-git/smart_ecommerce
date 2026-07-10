6fa3a22060177123798c488d6fc9a7002828ac97
Sumayya <saksumayya@gmail.com>
2026-07-10 18:22:14 +0530

    Add Celery background tasks and Resend email integration

--- store/api/resend.py
+++ store/api/resend.py
@@ -19,18 +19,24 @@ def send_resend_email(to_email, subject, html_content):
 
     try:
         response = requests.post(
-            url,
-            headers=headers,
-            json=data,
-            timeout=30,
-        )
+        url,
+        headers=headers,
+        json=data,
+        timeout=30,
+    )
+
+        print("Status:", response.status_code)
+        print("Body:", response.text)
 
         response.raise_for_status()
 
         print("✅ Resend email sent successfully.")
-
         return response.json()
 
     except requests.exceptions.RequestException as e:
         print("❌ Resend Email Error:", e)
+
+        if e.response:
+                print("Response:", e.response.text)
+
         raise
