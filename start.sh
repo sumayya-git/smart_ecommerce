3dff9b9f0b309b3e966212ef60765eb651dab26c
Sumayya <saksumayya@gmail.com>
2026-07-10 14:25:01 +0530

    Start Celery Worker

--- start.sh
+++ start.sh
@@ -10,6 +10,9 @@ python manage.py migrate --noinput
 echo "Setting nginx port..."
 sed -i "s/listen 80;/listen ${PORT};/" /etc/nginx/conf.d/default.conf
 
+echo "Starting Celery Worker..."
+celery -A ecommerce_project worker --loglevel=info &
+
 echo "Starting Gunicorn..."
 gunicorn ecommerce_project.wsgi:application \
     --bind 127.0.0.1:8000 &
