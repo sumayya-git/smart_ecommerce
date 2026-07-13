d28ade800a72104626d13c3c18c08471b69bede0
Sumayya <saksumayya@gmail.com>
2026-07-13 08:07:40 +0530

    fix url again

--- frontend-spa/src/pages/Checkout.js
+++ frontend-spa/src/pages/Checkout.js
@@ -132,7 +132,7 @@ function Checkout(){
 
                                 // Create Razorpay Order
                                 const paymentRes = await API.post(
-                                    "/create-order/",
+                                    "/payment/create-order/",
                                     {
                                         amount: totalAmount
                                     }
